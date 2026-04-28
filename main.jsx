4a4a9123fead4212e677e92a82a46edac0e2f225
thomashan17 <thomashan17@gmail.com>
2026-04-27 22:35:13 -0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ThomasHanPortfolio from "./ThomasHanPortfolio.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThomasHanPortfolio />
+  </React.StrictMode>
+);
