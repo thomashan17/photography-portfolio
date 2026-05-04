ea26890cb6699d56bcedecb0d5ac0a54eeba4d81
thomashan17 <thomashan17@gmail.com>
2026-05-03 17:57:19 -0700

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ThomasHanPortfolio from "./ThomasHanPortfolio.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ThomasHanPortfolio />
-  </React.StrictMode>
-);
